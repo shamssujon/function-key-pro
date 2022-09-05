a94412b93343ab33082e874643ab9ea798591d6a
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-09-05 10:21:10 +0600

    Ready to start project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
+        screens: {
+            sm: "576px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1160px",
+        },
+        container: {
+            center: true,
+            padding: "1.25rem",
+        },
         extend: {},
     },
     plugins: [],
